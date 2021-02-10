3854c2585b729eb51f31dd75f21d2037cad0d41a
dadatoa <heryra@gmail.com>
2021-02-10 20:40:49 +0100

    ajout d'une direction de repertoire dans settings.py STATIC_ROOT et installetion de django-pagedown

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'pagedown.apps.PagedownConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "~/Dropbox/webprojects/dadatoa.net/static/"
 
 LOGIN_REDIRECT_URL = '/'
 
